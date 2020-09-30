cd7c8f00b5c61f1538353ca0b41639e5fc4bc49a
JJ Asghar <jja@ibm.com>
2020-09-30 17:20:22 -0500

    Create check_url.sh

---
+++ check_url.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check for missing parameters
+if [[ -z "$1" ]]
+then
+        echo "Missing parameters! Syntax: ./check_url <url>"
+        exit 3
+fi
+
+url=$1
+code=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
+if [ $code -eq '200' ]
+then
+    echo "OK, $url is a 200"
+    exit 0
+else
+    echo "CRITICAL, $url isn't there"
+    exit 2
+fi
